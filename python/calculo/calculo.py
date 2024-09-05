5d6e8bf3670314593be2a85db2de190d19d33621
Diogo <diogofrmota@gmail.com>
2024-09-05 10:19:48 +0100

    primeira parte da implementação  do programa

--- python/calculo/calculo.py
+++ python/calculo/calculo.py
@@ -4,8 +4,26 @@ def main() -> None:
     jogar(pontos)
 
 def jogar(pontos: int) -> None:
-    pass
+    dificuldade: int = int(input('Informe o nível de dificuldade desejado [1, 2, 3 ou 4]: '))
 
+    calc: Conta = Conta(dificuldade)
+
+    print('Informe o resultado para a seguinte operação: ')
+    calc.mostrar_operacao()
+
+    resultado: int = int(input())
+
+    if calc.checar_resultado(resultado):
+        pontos += 1
+        print(f'Tem {pontos} ponto(s).')
+
+    continuar: int = int(input('Deseja continuar no jogo? [1 - sim, 0 não]'))
+
+    if continuar:
+        jogar(pontos)
+    else:
+        print(f'Finalizou com {pontos} ponto(s).')
+        print('Até à próxima!')
 
 if __name__ == '__main__':
     main()
