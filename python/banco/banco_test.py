ce5dfed5e7748a5fde09346feabb10ad9bf78edb
Diogo <diogofrmota@gmail.com>
2024-09-10 23:56:42 +0100

    Code testing

---
+++ python/banco/banco_test.py
@@ -0,0 +1,6 @@
+from models.cliente import Cliente
+from models.conta import Conta
+
+felicity: Cliente = Cliente('Felicity Jones', 'felicity@gmail.com', '123.456.789-01', '02/09/1987')
+
+angelina: Cliente = Cliente('Angelina Jolie', 'angelinay@gmail.com', '234.567.890-02', '08/07/1978')
