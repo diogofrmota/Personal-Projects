6a80ec3f4838c087723f962a02ff7a50c8695a12
Diogo <diogofrmota@gmail.com>
2024-10-02 00:31:40 +0100

    Updated helper.py

--- python/bank/utils/helper.py
+++ python/bank/utils/helper.py
@@ -1,14 +1,10 @@
-from datetime import date
-from datetime import datetime
+from datetime import date, datetime
 
+def date_to_str(date_obj: date) -> str:
+    return date_obj.strftime('%d/%m/%Y')
 
-def date_para_str(data: date) -> str:
-    return data.strftime('%d/%m/%Y')
+def str_to_date(date_string: str) -> date:
+    return datetime.strptime(date_string, '%d/%m/%Y')
 
-
-def str_para_date(data: str) -> date:
-    return datetime.strptime(data, '%d/%m/%Y')
-
-
-def formata_float_str_moeda(valor: float) -> str:
-    return f'{valor:,.2f} €'
+def format_float_to_currency(value: float) -> str:
+    return f'{value:,.2f}€'
